c6f64fb1046c94af2bb9a8419d47addb95efa38f
Jose Manuel Torres Oceguera <manuel789@live.com.mx>
2021-05-04 09:05:29 -0500

    Agregado Request para agregar nombres

--- src/main/java/mx/edu/itlapiedad/controladores/Mensaje.java
+++ src/main/java/mx/edu/itlapiedad/controladores/Mensaje.java
@@ -2,6 +2,7 @@ package mx.edu.itlapiedad.controladores;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class Mensaje {
 	@GetMapping("/hola")
-public String mostrar_hola(){
-	return"Hola devops";
+	
+public String mostrar_hola(@RequestParam String nombre){
+		
+	return"Hola "+nombre;
 }
 }
